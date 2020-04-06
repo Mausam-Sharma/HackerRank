ecd8d58a3391bd79bc818cba4cf9055451559613
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2020-04-06 17:25:05 +0530

    Beautiful Days at Movies

---
+++ src/Beautiful Days at Movies/Solution.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the beautifulDays function below.
+    static int beautifulDays(int i, int j, int k) {
+    	int count =0;
+    	for (int x = i; x <=j; x++) {
+			int temp = 0;
+			StringBuilder rev = new StringBuilder(Integer.toString(x));
+			temp = Integer.parseInt(rev.reverse().toString());
+			
+			if((Math.abs(x-temp)%k)==0) {
+				count++;
+			}
+		}
+    	
+    	return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] ijk = scanner.nextLine().split(" ");
+
+        int i = Integer.parseInt(ijk[0]);
+
+        int j = Integer.parseInt(ijk[1]);
+
+        int k = Integer.parseInt(ijk[2]);
+
+        int result = beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
